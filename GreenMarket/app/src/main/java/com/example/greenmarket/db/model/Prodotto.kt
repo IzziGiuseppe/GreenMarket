73588b2cc32d5c1edfb07b7e993ebabc933ac4cb
domenicolaporta00 <s1095492@studenti.univpm.it>
2024-05-17 10:58:01 +0200

    Aggiornato DB; prove utilizzo entità prodotto

--- GreenMarket/app/src/main/java/com/example/greenmarket/db/model/Prodotto.kt
+++ GreenMarket/app/src/main/java/com/example/greenmarket/db/model/Prodotto.kt
@@ -4,10 +4,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "prodotto")
-data class Prodotto (
+data class Prodotto(
     @PrimaryKey val nome: String,
     val descrzione: String,
-    val prezzo: String,
+    val prezzo: Float,
     val foto: String,
     val unita_di_misura: String
 )
