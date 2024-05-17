3ca4aa317e26cf590153e61f4ceb53b06d7f5110
domenicolaporta00 <s1095492@studenti.univpm.it>
2024-05-17 12:18:48 +0200

    Il DB funziona e le istanze di prodotto vengono visualizzate correttamente.

--- GreenMarket/app/src/main/java/com/example/greenmarket/ui/home/HomeFragment.kt
+++ GreenMarket/app/src/main/java/com/example/greenmarket/ui/home/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.greenmarket.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.greenmarket.databinding.FragmentHomeBinding
 import com.example.greenmarket.db.model.Prodotto
@@ -18,29 +21,47 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        Log.d("HomeFragment", "Ciao")
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val prod1 = Prodotto("Pera", "Ciao", 10f, "", "kg")
-        val prod2 = Prodotto("Banana", "Ciao", 10f, "", "kg")
-        val prod3 = Prodotto("Arancia", "Ciao", 10f, "", "kg")
+        Log.d("HomeFragment", "Ciao")
+
+        val prod1 = Prodotto("Zucchine", "Ciao", 10f, "", "kg")
+        Log.d("HomeFragment", "Ciao3")
+        val prod2 = Prodotto("Melanzane", "Ciao", 10f, "", "kg")
+        Log.d("HomeFragment", "Ciao4")
+        val prod3 = Prodotto("Carne", "Ciao", 10f, "", "kg")
+
+        Log.d("HomeFragment", "Ciao")
 
         homeViewModel.insert(prod1, prod2, prod3)
+
+        Log.d("HomeFragment", "Ciao5")
 
         val variabile = homeViewModel.readAllStudents()
 
         val textView: TextView = binding.textHome
-        homeViewModel.listaProdotti.observe(viewLifecycleOwner) {
-            textView.text = variabile.toString()
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
         }
+
+        val studentListObserver = Observer<Array<Prodotto>> {
+            /*for(prod in it)
+                Log.d("MainActivity","${prod.nome} ${prod.descrzione} ${prod.prezzo}")*/
+            textView.text = it[0].nome + ", " + it[1].nome + ", " + it[2].nome
+        }
+        homeViewModel.listaProdotti.observe(viewLifecycleOwner, studentListObserver)
 
         return root
     }
