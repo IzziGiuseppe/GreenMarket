fd523465ccddd3686b16e88192aac8b412339e9d
IzziGiuseppe <s1094052@studenti.univpm.it>
2024-05-17 10:03:24 +0200

    Update DB

---
+++ GreenMarket/app/src/main/java/com/example/greenmarket/db/GMDatabase.kt
@@ -0,0 +1,59 @@
+package com.example.greenmarket.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.greenmarket.db.dao.CodiceScontoDao
+import com.example.greenmarket.db.dao.ComposizioneScontriniDao
+import com.example.greenmarket.db.dao.ProdottiInRicetteDao
+import com.example.greenmarket.db.dao.ProdottoDao
+import com.example.greenmarket.db.dao.RicettaDao
+import com.example.greenmarket.db.dao.ScontrinoDao
+import com.example.greenmarket.db.dao.TesseraAPuntiDao
+import com.example.greenmarket.db.dao.UtenteDao
+import com.example.greenmarket.db.model.CodiceSconto
+import com.example.greenmarket.db.model.ComposizioneScontrini
+import com.example.greenmarket.db.model.ProdottiInRicette
+import com.example.greenmarket.db.model.Prodotto
+import com.example.greenmarket.db.model.Ricetta
+import com.example.greenmarket.db.model.Scontrino
+import com.example.greenmarket.db.model.TesseraAPunti
+import com.example.greenmarket.db.model.Utente
+
+@Database(entities = [Utente::class, TesseraAPunti::class, Scontrino::class, Ricetta::class, Prodotto::class,
+    ProdottiInRicette::class, ComposizioneScontrini::class, CodiceSconto::class], version=1)
+abstract class GMDatabase: RoomDatabase() {
+    abstract fun UtenteDao(): UtenteDao
+    abstract fun TesseraAPuntiDao(): TesseraAPuntiDao
+
+    abstract fun ScontrinoDao(): ScontrinoDao
+
+    abstract fun RicettaDao(): RicettaDao
+
+    abstract fun ProdottoDao(): ProdottoDao
+
+    abstract fun ProdottiInRicetteDao(): ProdottiInRicetteDao
+
+    abstract fun ComposizioneScontriniDao(): ComposizioneScontriniDao
+
+    abstract fun CodiceScontoDao(): CodiceScontoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: GMDatabase? = null
+
+        fun getInstance(context: Context): GMDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    GMDatabase::class.java, "green_market_database"
+                ).allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
+
+}
