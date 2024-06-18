6c27149eaac3cadf16e2610e5bf341c28b076483
domenicolaporta00 <s1095492@studenti.univpm.it>
2024-06-18 18:11:32 +0200

    Implementato easter egg. Vediamo se riuscite a scoprirlo :)

--- GreenMarket/app/src/main/java/com/example/greenmarket/SplashActivity.kt
+++ GreenMarket/app/src/main/java/com/example/greenmarket/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.greenmarket
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -24,8 +27,26 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             }
-            startActivity(intent)
+            val intentNoInternet = Intent(this, NoInternetActivity::class.java)
+            if (isInternetAvailable(this)) {
+                startActivity(intent)
+            }
+            else {
+                startActivity(intentNoInternet)
+            }
             finish()
         }, 3000)
+    }
+
+    private fun isInternetAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
     }
 }
